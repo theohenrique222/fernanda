0e82f53fe0945c5ef091aa4703bc029e3aa00e6c
theohenrique222 <theodoro222@hotmail.com>
2025-11-22 15:18:51 -0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<template>
+  <h1>You did it!</h1>
+  <p class="bg-red-600">
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>
+</template>
+
+<style scoped></style>
