e2f011236853b0b6fd196df9e92187c507b6f694
theohenrique222 <theodoro222@hotmail.com>
2025-11-23 17:51:21 -0300

    create about section

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script setup>
   import navbar from './components/navbar.vue';
   import hero from './pages/hero.vue';
+  import about from './pages/about.vue';
 </script>
 
 <template>
   <div>
     <navbar/>
     <hero/>
+    <about/>
   </div>
 </template>
 
---
+++ src/pages/about.vue
@@ -0,0 +1,26 @@
+<template>
+    <section class="min-h-screen bg-amber-50">
+        <main class="max-w-6xl m-auto flex">
+            <div class="max-w-4xl">
+                <h1 class="text-red-950 text-4xl font-extrabold mb-10">Bem-estar que transforma!</h1>
+                <p class="text-black text-xl font-extralight">Terapeuta integrativa com ampla experiência em tratamentos
+                    corporais voltados ao bem-estar físico e emocional. Especialista
+                    em técnicas de relaxamento, incluindo massagem relaxante,
+                    ventosaterapia, liberação miofascial e dry needling, com foco em
+                    abordagens humanizadas para alívio da dor. Também atua em
+                    tratamentos estéticos funcionais, como drenagem linfática,
+                    massagem modeladora e massagem esculpe detox,
+                    proporcionando resultados eficazes e cuidados personalizados.</p>
+            </div>
+            <div class="">
+                <h1 class="text-3xl text-black">IMG</h1>
+            </div>
+        </main>
+    </section>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style></style>
