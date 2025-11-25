fa46a788cd425cf51c43fb3c761832d7451723d9
theohenrique222 <theodoro222@hotmail.com>
2025-11-25 19:58:07 -0300

    add mobile response in end section

--- src/pages/end.vue
+++ src/pages/end.vue
@@ -2,14 +2,14 @@
     <section class="bg-amber-50 text-red-950">
         <main class="pt-28 text-center max-w-6xl m-auto">
             <div class="mb-20">
-                <h1 class="text-red-950  text-5xl">
+                <h1 class="text-red-950 lg:text-5xl text-xl">
                     Valorize-se: corpo e mente em equilíbrio são
                     seu melhor investimento.
                 </h1>
             </div>
             
             <div class="max-w-2xl m-auto pb-52">
-                <h2 class="uppercase text-3xl font-extralight">
+                <h2 class="uppercase lg:text-3xl text-lg font-extralight">
                     SEJA SUA PRIORIDADE!
                 </h2>
             </div>
