673aca0547098209aa06b44fb384c95e99add128
theohenrique222 <theodoro222@hotmail.com>
2025-11-25 19:05:06 -0300

    add mobile response in hero section

--- src/pages/hero.vue
+++ src/pages/hero.vue
@@ -1,11 +1,11 @@
 <template>
-    <section id="home" class="bg-amber-50 min-h-screen text-red-950">
+    <section id="home" class="bg-amber-50 text-red-950 pb-20">
         <main class="pt-16 text-center max-w-6xl m-auto">
             <div class="pt-10 mb-4 self-auto">
                 <img class="h-56 mx-auto" src="/img/logo.png" alt="Logo">
             </div>
             <div class="mb-2">
-                <h1 class="text-textcolor font-extrabold text-7xl">
+                <h1 class="text-textcolor font-extrabold lg:text-7xl md:text-5xl text-4xl">
                     Fernanda Monteiro
                 </h1>
             </div>
@@ -15,7 +15,7 @@
                 </h3>
             </div>
             <div class="max-w-2xl m-auto">
-                <h2 class="uppercase text-3xl font-extralight">
+                <h2 class="uppercase lg:text-3xl text-lg font-extralight">
                     TRANSFORME SEU CUIDADO EM PRIORIDADE!
                 </h2>
             </div>
