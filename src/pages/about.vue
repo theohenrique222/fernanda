afa88e0c49c5df106bbab1e36eda421f7c3851f9
Theo Henrique <theodoro222@hotmail.com>
2025-11-26 22:55:42 -0300

    correction margins about section

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -1,17 +1,27 @@
 <template>
     <section id="about" class="bg-amber-50 pb-20">
-        <main class="max-w-6xl m-auto flex flex-col lg:flex-row items-start gap-10 px-4">
+        <main class="max-w-6xl px-5 m-auto flex flex-col lg:flex-row items-start gap-10">
 
             <div class="flex-1">
-                <h1 class="text-red-950 lg:text-4xl text-2xl font-extrabold mb-16 mt-30">Bem-estar que transforma!</h1>
-                <p class="text-black lg:text-xl text-xs font-extralight">Terapeuta integrativa com ampla experiência em tratamentos
-                    corporais voltados ao bem-estar físico e emocional. Especialista
-                    em técnicas de relaxamento, incluindo massagem relaxante,
-                    ventosaterapia, liberação miofascial e dry needling, com foco em
-                    abordagens humanizadas para alívio da dor. Também atua em
-                    tratamentos estéticos funcionais, como drenagem linfática,
-                    massagem modeladora e massagem esculpe detox,
-                    proporcionando resultados eficazes e cuidados personalizados.</p>
+                <h1 class="text-red-950 lg:text-4xl text-2xl font-extrabold lg:mb-16 mb-3 mt-30">Bem-estar que transforma!</h1>
+                <div class="text-black lg:text-xl text-xs font-extralight space-y-2">
+                    <p>
+                        Terapeuta integrativa com ampla experiência em tratamentos
+                        corporais voltados ao bem-estar físico e emocional.
+                    </p>
+                    <p>
+                        Especialista
+                        em técnicas de relaxamento, incluindo massagem relaxante,
+                        ventosaterapia, liberação miofascial e dry needling, com foco em
+                        abordagens humanizadas para alívio da dor. 
+                    </p>
+                    <p>
+                        Também atua em
+                        tratamentos estéticos funcionais, como drenagem linfática,
+                        massagem modeladora e massagem esculpe detox,
+                        proporcionando resultados eficazes e cuidados personalizados.
+                    </p>
+                </div>
             </div>
 
             <div class="flex m-auto">
