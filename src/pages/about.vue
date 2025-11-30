77399158ab0090838dcb98c90311eec86623a47c
Theo Henrique <theodoro222@hotmail.com>
2025-11-30 17:20:02 -0300

    add animation in about section

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -3,8 +3,12 @@
         <main class="max-w-6xl px-5 m-auto flex flex-col lg:flex-row items-start gap-10">
 
             <div class="flex-1">
-                <h1 class="text-red-950 lg:text-4xl text-2xl font-extrabold lg:mb-16 mb-3 mt-30">Bem-estar que transforma!</h1>
-                <div class="text-black lg:text-xl text-xs font-extralight space-y-2">
+                <h1 class="text-red-950 lg:text-4xl text-2xl font-extrabold lg:mb-16 mb-3 mt-30"
+                    v-animateonscroll="{ enterClass: 'opacity-0 animate-[fadeIn_1s_ease_forwards]' }">
+                    Bem-estar que transforma!
+                </h1>
+                <div class="text-black lg:text-xl text-xs font-extralight space-y-2"
+                    v-animateonscroll="{ enterClass: 'opacity-0 animate-[fadeIn_1.5s_ease_forwards]' }">
                     <p>
                         Terapeuta integrativa com ampla experiência em tratamentos
                         corporais voltados ao bem-estar físico e emocional.
@@ -24,7 +28,8 @@
                 </div>
             </div>
 
-            <div class="flex m-auto">
+            <div class="flex m-auto"
+                v-animateonscroll="{ enterClass: 'opacity-0 animate-[fadeIn_2s_ease_forwards]' }">
                 <img class="lg:h-screen h-96" src="/img/avatar.png" alt="">
             </div>
         </main>
