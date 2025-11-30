4730ec58d0dc35643c65ed3203cf889238e3c433
Theo Henrique <theodoro222@hotmail.com>
2025-11-30 17:17:18 -0300

    add animation hero section

--- src/main.js
+++ src/main.js
@@ -1,35 +1,26 @@
-import { createApp } from 'vue'
+import { createApp } from 'vue';
 import PrimeVue from 'primevue/config';
 import Aura from '@primeuix/themes/aura';
+import AnimateOnScroll from 'primevue/animateonscroll';
 import Button from 'primevue/button';
-import 'primeicons/primeicons.css'
 import Menubar from 'primevue/menubar';
 import AutoComplete from 'primevue/autocomplete';
-import AnimateOnScroll from 'primevue/animateonscroll';
+
+import 'primeicons/primeicons.css';
 import App from './App.vue';
 
 const app = createApp(App);
 
 app.use(PrimeVue, {
     theme: {
-        preset: Aura,
-        options: {
-            prefix: 'p',
-            darkMode: false,
-            cssLayer: {
-                name: 'primevue',
-                order: 'tailwind-base, primevue, tailwind-utilities'
-            },
-            
-            includeDarkVariants: false,
-            presetName: 'aura',
-            presetValue: 'light'
-        }
+        preset: Aura
     }
 });
 
 app.directive('animateonscroll', AnimateOnScroll);
-app.component('AutoComplete', AutoComplete);
+
 app.component('Button', Button);
 app.component('Menubar', Menubar);
+app.component('AutoComplete', AutoComplete);
+
 app.mount('#app');
--- src/pages/hero.vue
+++ src/pages/hero.vue
@@ -2,25 +2,28 @@
     <section id="home" class="bg-amber-50 min-h-screen text-red-950">
         <main 
             class="pt-24 text-center max-w-6xl m-auto"
-            v-animateonscroll="{ enterClass: 'animate-enter fade-in-10 slide-in-from-l-8 animate-duration-1000', leaveClass: 'animate-leave fade-out-0' }"
             >
             <div
+                v-animateonscroll="{ enterClass: 'opacity-0 animate-[fadeIn_0.8s_ease_forwards]' }"
                 class="pt-10 mb-4 self-auto">
                 <a href="/">
                     <img class="lg:h-56 h-46 mx-auto" src="/img/logo.png" alt="Logo">
                 </a>
             </div>
-            <div class="mb-2">
+            <div class="mb-2"
+                v-animateonscroll="{ enterClass: 'opacity-0 animate-[fadeIn_1s_ease_forwards]' }">
                 <h1 class="text-textcolor font-extrabold lg:text-7xl md:text-5xl text-3xl">
                     Fernanda Monteiro
                 </h1>
             </div>
-            <div class="mb-16">
+            <div class="mb-16"
+                v-animateonscroll="{ enterClass: 'opacity-0 animate-[fadeIn_2.0s_ease_forwards]' }">
                 <h3 class="lg:text-xl text-base uppercase font-extralight">
                     Terapeuta Integrativa
                 </h3>
             </div>
-            <div class="max-w-2xl m-auto">
+            <div class="max-w-2xl m-auto"
+                v-animateonscroll="{ enterClass: 'opacity-0 animate-[fadeIn_2.5s_ease_forwards]' }">
                 <h2 class="uppercase lg:text-3xl text-lg lg:px-0 px-5 font-extralight">
                     TRANSFORME SEU CUIDADO EM PRIORIDADE!
                 </h2>
